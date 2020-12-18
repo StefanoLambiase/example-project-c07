0e4a365f2f548d0548722048cadaee3855dbeae2
Stefano Lambiase <stefanolambiase96@gmail.com>
2020-12-18 12:42:09 +0100

    Update LibroServiceImplTest.java

--- src/test/java/it/unisa/c07/exampleprojectc07/libro/service/LibroServiceImplTest.java
+++ src/test/java/it/unisa/c07/exampleprojectc07/libro/service/LibroServiceImplTest.java
@@ -4,19 +4,14 @@ import it.unisa.c07.exampleprojectc07.entity.Libro;
 import it.unisa.c07.exampleprojectc07.libro.dao.LibroDAO;
 
 import org.junit.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -33,7 +28,7 @@ public class LibroServiceImplTest {
     private LibroDAO libroDAO;
 
     @Test
-    public void findAllLbiro() {
+    public void findAllLibro() {
         Libro libro = new Libro();
         List<Libro> libroList = new ArrayList<>();
         libroList.add(libro);
